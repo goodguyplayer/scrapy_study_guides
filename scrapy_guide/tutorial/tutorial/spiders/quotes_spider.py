bad8d0003ecbdb5484c1f08cc86f9b59bb376ebb
goodguyplayer <nathanbritodasilva@gmail.com>
2024-04-22 15:44:26 -0300

    spider_quotes updated

--- scrapy_guide/tutorial/tutorial/spiders/quotes_spider.py
+++ scrapy_guide/tutorial/tutorial/spiders/quotes_spider.py
@@ -5,26 +5,18 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-
-    def start_requests(self):
-        urls = [
-            "https://quotes.toscrape.com/page/1/",
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [
+        'http://quotes.toscrape.com/',
+    ]
 
     def parse(self, response):
         for quote in response.css("div.quote"):
             yield {
-                "text": quote.css("span.text::text").get(),
-                "author": quote.css("small.author::text").get(),
-                "tags": quote.css("div.tags a.tag::text").getall(),
+                'text': quote.css("span.text::text").extract_first(),
+                'author': quote.css("small.author::text").extract_first(),
+                'tags': quote.css("div.tags > a.tag::text").extract()
             }
-        next_page = response.css("li.next a::atr(href)").get()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
-        #page = response.url.split("/")[-2]
-        #filename = f"quotes-{page}.html"
-        #Path(filename).write_bytes(response.body)
-        #self.log(f"Saved file {filename}")
+
+        next_page_url = response.css("li.next > a::attr(href)").extract_first()
+        if next_page_url is not None:
+            yield scrapy.Request(response.urljoin(next_page_url))
