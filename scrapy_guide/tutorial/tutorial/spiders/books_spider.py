78012ba87b3e255d872c5e8c6ed6b2c8f3212af8
goodguyplayer <nathanbritodasilva@gmail.com>
2024-04-22 16:57:55 -0300

    spider_books - updated w more params
    - Title updated
    - Rating updated
    - url added
    - image_url added
    - price added

--- scrapy_guide/tutorial/tutorial/spiders/books_spider.py
+++ scrapy_guide/tutorial/tutorial/spiders/books_spider.py
@@ -12,6 +12,10 @@ class BooksSpider(scrapy.Spider):
     def parse(self, response):
         for book in response.css("article.product_pod"):
             yield {
-                'title': book.css('h3 a').attrib['title'],
-                'rating': book.css('p').attrib['class'],
+                'title': book.css('h3 a::attr(title)').extract_first(),
+                'rating': book.css('p::attr(class)').extract_first(),
+                'url': response.urljoin(book.css('h3 a::attr(href)').extract_first()),
+                'image_url' : response.urljoin(book.css('div.image_container a img::attr(src)').extract_first()),
+                'price' : book.css('div.product_price p.price_color::text').extract_first(),
+                #'is_instock' : "",
             }
