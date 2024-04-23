cafd1ba6e0a897b255128b76287b0daad4497f15
goodguyplayer <nathanbritodasilva@gmail.com>
2024-04-23 08:53:44 -0300

    itemsloader update - more variables
    included url_in & image_url_in

--- scrapy_guide/tutorial/tutorial/itemsloaders.py
+++ scrapy_guide/tutorial/tutorial/itemsloaders.py
@@ -4,3 +4,5 @@ from scrapy.loader import ItemLoader
 class BookItemLoader(ItemLoader):
     default_output_processor = TakeFirst()
     price_in = MapCompose(lambda x: x.split("£")[-1])
+    url_in = MapCompose(lambda x: 'https://books.toscrape.com/' + x)
+    image_url_in = MapCompose(lambda x: 'https://books.toscrape.com' + (x.replace("..", "")))
