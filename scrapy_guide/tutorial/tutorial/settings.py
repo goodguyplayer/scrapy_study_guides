7df4b53bb4bf24f678b73bdd43ba9cc25e13afa3
goodguyplayer <nathanbritodasilva@gmail.com>
2024-04-23 11:48:15 -0300

    Settings updated - Useragent add

--- scrapy_guide/tutorial/tutorial/settings.py
+++ scrapy_guide/tutorial/tutorial/settings.py
@@ -20,7 +20,7 @@ NEWSPIDER_MODULE = "tutorial.spiders"
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -94,3 +94,8 @@ ITEM_PIPELINES = {
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
